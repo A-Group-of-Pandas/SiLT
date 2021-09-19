f8db4397e2d452153eb73d0757a9a9b23af3d583
kw-0 <46660348+kw-0@users.noreply.github.com>
2021-09-19 10:37:48 -0400

    improves ASL grammar data generation

--- ASL_grammar_generation/ASL_grammar_data_gen.py
+++ ASL_grammar_generation/ASL_grammar_data_gen.py
@@ -1,50 +1,98 @@
 import random
 from pathlib import Path
+from typing import Union, Tuple
+import re
+import numpy as np
 
-corpus_path = Path("ASL_grammar_generation/wikipedia2text-extracted.txt")
+"""
+TO DO:
+1) Get a better corpus
+2) finish up function creation based on mygrad stuff
+3) remove non-ASL words (before randomizing) and maybe choose a very small number to not randomize
+4) build basline RNN for ASL grammar to normal english grammar
+5) look into attention mechanism based RNN and other better ways of grammar translation
 
-# x and y here refer to training and testing respectively
-corpus_X_input_path = Path("ASL_grammar_generation/x_input_data.txt")
-corpus_Y_input_path = Path("ASL_grammar_generation/y_input_data.txt")
-
-corpus_X_truth_path = Path("ASL_grammar_generation/x_truth_data.txt")
-corpus_Y_truth_path = Path("ASL_grammar_generation/y_truth_data.txt")
-
-split_value = 0.8
-
-# reads the corpus
-with open(corpus_path, mode="r") as corpus:
-    corpus = corpus.read().lower()
-
-# removes new lines and apostrophes from the corpus
-removable_punc = """"#$%&'()*+,-/:;<=>@[\]^_`{|}~'"""
-corpus = corpus.translate(str.maketrans("", "", removable_punc))
+IDEAS: 
+- Could we use a linear parse tree parts of speech (a.k.a POS) rnn (trained with a parse tree tagged dataset) along with the grammar translator to improve preformance <https://proceedings.neurips.cc/paper/2015/file/277281aada22045c03945dcb2ca6f2ec-Paper.pdf>
+- Sometimes maybe have it pass in a perfect grammar sentence so that it learns to ignore perfect english sentences too
+"""
 
 
-truth_sentences = corpus.split(". ")
-random.shuffle(truth_sentences)
+def english_to_ASL(
+    *,
+    corpus_filename: Union[str, Path],
+    non_ASL_filename: Union[str, Path],
+    x_inputs_filename: Union[str, Path] = "./x_inputs_data.txt",
+    y_inputs_filename: Union[str, Path] = "./y_inputs_data.txt",
+    x_truths_filename: Union[str, Path] = "./x_truths_data.txt",
+    y_truths_filename: Union[str, Path] = "./y_truth_data.txt",
+    SPLIT_VALUE: float = 0.8,
+) -> Tuple[list, list, list, list]:
+    """Turns a text corpus into pusedo-ASL grammar."""
 
-# randomizes the words in each sentence and saves it as a string where each new word-randomized sentence is on a newline
-input_sentences = [
-    " ".join(random.sample(sent.split(" "), len(sent.split(" "))))
-    for sent in truth_sentences
-]
+    corpus_file = Path(corpus_filename)
+    non_ASL_file = Path(non_ASL_filename)
 
-# this splits our data into train inputs, test inputs and train truths, test truths then turns it all into a string with each new scrambled sentence on a new line with a period at the end
-data_split = round(len(truth_sentences) * 0.8)
+    # x and y here refer to training and testing respectively
+    corpus_X_input_path = Path(x_inputs_filename)
+    corpus_Y_input_path = Path(y_inputs_filename)
+    corpus_X_truth_path = Path(x_truths_filename)
+    corpus_Y_truth_path = Path(y_truths_filename)
 
-X_input_sentences = ".\n".join(input_sentences[:data_split])
-Y_input_sentences = ".\n".join(input_sentences[data_split:])
+    # reads the corpus
+    with open(corpus_file, mode="r") as corpus:
+        corpus = corpus.read().lower()
 
-X_truth_sentences = ".\n".join(truth_sentences[:data_split])
-Y_truth_sentences = ".\n".join(truth_sentences[data_split:])
+    with open(non_ASL_file, mode="r") as removable_words:
+        removable_words = removable_words.read().lower()
 
-with open(corpus_X_input_path, mode="x") as _file:
+    # removes non-end punctuation from the corpus
+    mid_punc = """#$%&()*+,/:;<=>@[\]^_`"{|}~'"""
+    corpus = corpus.translate(str.maketrans("", "", mid_punc))
+
+    # splits and cleans the corpus
+    truth_sentences = corpus.split(". ")
+    truth_sentences = [sent.strip() for sent in truth_sentences]
+    random.shuffle(truth_sentences)
+
+    # randomly swaps around the words in each sentence
+    input_sentences = [
+        " ".join(random.sample(sent.split(" "), len(sent.split(" "))))
+        for sent in truth_sentences
+    ]
+
+    # this splits our data into train inputs, test inputs and train truths, test truths then turns it all into a string with each new scrambled sentence on a new line with a period at the end
+    data_split = round(len(truth_sentences) * SPLIT_VALUE)
+    X_input_sentences = ".\n".join(input_sentences[:data_split])
+    Y_input_sentences = ".\n".join(input_sentences[data_split:])
+    X_truth_sentences = ".\n".join(truth_sentences[:data_split])
+    Y_truth_sentences = ".\n".join(truth_sentences[data_split:])
+
+    # removes non ASL words from the input sentences
+    removable_words = removable_words.split("\n")
+    for word in removable_words:
+        X_input_sentences = re.sub(f" {word} ", " ", X_input_sentences)
+        Y_input_sentences = re.sub(f" {word} ", " ", Y_input_sentences)
+
+    return X_input_sentences, Y_input_sentences, X_truth_sentences, Y_truth_sentences
+
+
+non_ASL_path = Path("ASL_grammar_generation/non_ASL_words.txt")
+corpus_path = Path("ASL_grammar_generation/lung_cancer.txt")
+
+(
+    X_input_sentences,
+    Y_input_sentences,
+    X_truth_sentences,
+    Y_truth_sentences,
+) = english_to_ASL(corpus_filename=corpus_path, non_ASL_filename=non_ASL_path)
+
+with open("./x_input_data.txt", mode="w") as _file:
     _file.write(X_input_sentences)
-with open(corpus_Y_input_path, mode="x") as _file:
+with open("./y_input_data.txt", mode="w") as _file:
     _file.write(Y_input_sentences)
 
-with open(corpus_X_truth_path, mode="x") as _file:
+with open("./x_truth_data.txt", mode="w") as _file:
     _file.write(X_truth_sentences)
-with open(corpus_Y_truth_path, mode="x") as _file:
-    _file.write(Y_truth_sentences[data_split:])
+with open("./y_truth_data.txt", mode="w") as _file:
+    _file.write(Y_truth_sentences)
